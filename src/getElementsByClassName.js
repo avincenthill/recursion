9b857a2749191c538485675b1d2ee0e6fc9903bb
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-07-02 21:38:18 -0700

    Comment in requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,46 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function (className) {
   // your code here
 };
+
+/*
+var htmlStrings = [
+  '<div class="targetClassName"></div>',
+  '<div class="otherClassName targetClassName"></div>',
+  '<div><div class="targetClassName"></div></div>',
+  '<div><div class="targetClassName"><div class="targetClassName"></div></div></div>',
+  '<div><div></div><div><div class="targetClassName"></div></div></div>',
+  '<div><div class="targetClassName"></div><div class="targetClassName"></div></div>',
+  '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>'
+];
+
+describe('getElementsByClassName', function() {
+
+  it('should match the results of calling the built-in function', function() {
+    $('body').addClass('targetClassName');
+    htmlStrings.forEach(function(htmlString) {
+      var $rootElement = $(htmlString);
+      $('body').append($rootElement);
+
+      var result = getElementsByClassName('targetClassName');
+      var expectedNodeList = document.getElementsByClassName('targetClassName');
+      var expectedArray = Array.prototype.slice.apply(expectedNodeList);
+      var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
+      expect(equality).to.equal(true);
+
+      $rootElement.remove();
+    });
+    $('body').removeClass('targetClassName');
+  });
+
+});
+*/
+
+/*
+Bare minimum Requirements
+Replace stringifyJSON with your own function in src/stringifyJSON.js, and make the specs pass.
+Implement getElementsByClassName with your own function in src/getElementsByClassName.js, and make the specs pass.
+You should use document.body, element.childNodes, and element.classList
+*/
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,17 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
-var parseJSON = function(json) {
+var parseJSON = function (json) {
   // your code goes here
 };
+
+/*
+Advanced Content
+Our advanced content is intended to throw you in over your head, requiring you to solve problems with very little support or oversight, much like you would as a mid or senior level engineer. The following problem is no exception, and you may have to do a fair amount of work to get enough context to get started on the problem itself.
+
+Replace parseJSON with your own function in src/parseJSON.js, and make the specs pass.
+Use a recursive descent parser.
+Resources:
+One of Hack Reactor's amazing graduates, Ron Fenolio, wrote a fabulous blog post after wrestling with this very prompt
+Note: This is a lot of work, and you should expect (and look forward) to bumping up against several conceptual hurdles
+*/
